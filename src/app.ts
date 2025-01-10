43ecdc5a2c1f5afc2eaef96f091ed708219456bb
Nare-Stepanyan <narestepanyan90@gmail.com>
2025-01-10 16:45:17 +0400

    TDA-001: Set up Node.js server with Express and TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.get('/about', (req: Request, res: Response) => {
+    res.status(200).json({message: 'This is an application that interacts with twitter apis'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
