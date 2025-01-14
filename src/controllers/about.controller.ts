46f466e0517a731a817b00bb98b0cf52cb554401
Nare-Stepanyan <narestepanyan90@gmail.com>
2025-01-14 20:19:10 +0400

    TDA-001: -rename getAbout controller to aboutHandler

--- src/controllers/about.controller.ts
+++ src/controllers/about.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
-export const getAbout = (_req: Request, res: Response) => {
-    res.status(200).json({message: 'This is an application that interacts with twitter apis'});
+export const aboutHandler = (request: Request, response: Response) => {
+    response.status(200).json({message: 'This is an application that interacts with twitter apis'});
 };
--- src/routes/about-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAbout } from "../controllers/about.controller";
-
-const router = Router();
-
-router.get("/", getAbout);
-
-export default router;
