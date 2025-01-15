f56c17093a623a0d176014cf6e6dfad8b67726cd
Nare-Stepanyan <narestepanyan90@gmail.com>
2025-01-15 20:53:04 +0400

    feat: setup prettier and run

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-import express from "express";
-import aboutRouter from "./routes/about-routes";
+import dotenv from 'dotenv';
+import express from 'express';
+import aboutRouter from './routes/about-routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/about", aboutRouter);
+app.use('/about', aboutRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
